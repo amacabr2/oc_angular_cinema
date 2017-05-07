72ffc713c18e523550917c79d377557708b60699
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2017-05-07 13:59:05 +0200

    Controller pour faire des recherches de films

---
+++ app/scripts/controllers/header.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cineAngularApp.controller:HeaderCtrl
+ * @description
+ * # HeaderCtrl
+ * Controller of the cineAngularApp
+ */
+angular.module('cineAngularApp').controller('HeaderCtrl', function ($scope, $location) {
+
+    $scope.query = "";
+
+    $scope.searchAction = _ => {
+      $location.path("/search/" + $scope.query);
+    };
+
+});
