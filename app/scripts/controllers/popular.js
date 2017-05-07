0a6e4cce199baefc2fb6923a3fa264ba52431df2
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2017-05-07 10:12:10 +0200

    Le controlleur récupère les infos et s'occupe du changement de page

--- app/scripts/controllers/popular.js
+++ app/scripts/controllers/popular.js
@@ -7,11 +7,24 @@
  * # PopularCtrl
  * Controller of the cineAngularApp
  */
-angular.module('cineAngularApp')
-  .controller('PopularCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+angular.module('cineAngularApp').controller('PopularCtrl', function () {
+
+    $scope.currentPage = 1;
+    $scope.totalPages = 0;
+
+    let loadMovies = _ => {
+        $scope.loading = true;
+        serviceAjax.popular($scope.currentPage).success((data) => {
+           $scope.movies = data.results;
+           $scope.totalPages = data.total_pages;
+           $scope.loading = false;
+        });
+    };
+
+    $scope.pageChanged = _ => {
+        loadMovies();
+    };
+
+    loadMovies();
+
+});
