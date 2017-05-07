4a3cbc3e6c42c4e88c7fb78e65661879f1c70690
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2017-05-07 14:07:39 +0200

    Controlleur pour rechercher les films

--- app/scripts/controllers/search.js
+++ app/scripts/controllers/search.js
@@ -7,11 +7,23 @@
  * # SearchCtrl
  * Controller of the cineAngularApp
  */
-angular.module('cineAngularApp')
-  .controller('SearchCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+angular.module('cineAngularApp').controller('SearchCtrl', function ($scope, $routeParams, serviceAjax) {
+
+    $scope.query = $routeParams.query;
+    $scope.currentPage = 1;
+    $scope.totalPages = 0;
+
+    let loadMovies = _ => {
+      serviceAjax.search($scope.query, $scope.currentPage).then((data) => {
+         $scope.movies = data.data.results;
+         $scope.totalPages = data.data.total_pages;
+      });
+    };
+
+    $scope.pageChanged = _ => {
+      loadMovies();
+    };
+
+    loadMovies();
+
+});
