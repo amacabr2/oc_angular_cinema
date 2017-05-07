076227239e6b42b89dedd9810ed06eaab4c18c6a
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2017-05-07 15:05:36 +0200

    test requete ajax pour la recherche de film

--- test/spec/services/serviceajax.js
+++ test/spec/services/serviceajax.js
@@ -2,21 +2,38 @@
 
 describe('Service: serviceAjax', function () {
 
-  // load the service's module
-  beforeEach(module('cineAngularApp'));
+    // load the service's module
+    beforeEach(module('cineAngularApp'));
 
-  // instantiate service
-  let serviceAjax, httpBackend, popularRequest;
+    // instantiate service
+    let serviceAjax, httpBackend, popularRequest;
 
-  beforeEach(inject(function (_serviceAjax_, _$httpBackend_) {
-    serviceAjax = _serviceAjax_;
-    httpBackend = _$httpBackend_
-  }));
+    beforeEach(inject(function (_serviceAjax_, _$httpBackend_) {
+        serviceAjax = _serviceAjax_;
+        httpBackend = _$httpBackend_
+    }));
 
-  it('should make a request to the right URL when calling popular function', function () {
-    serviceAjax.popular(1);
-    httpBackend.expectGET('http://localhost:3000/popular?page=1').respond({});
-    httpBackend.flush();
-  });
+    it('should make a request to the right URL when calling popular function', function () {
+        serviceAjax.popular(1);
+        httpBackend.expectGET('http://localhost:3000/popular?page=1').respond({});
+        httpBackend.flush();
+    });
+
+    it('should set $scope.movies and $scope.total_pages when calling $scope.loadMovies', function () {
+
+        spyOn(serviceAjax, 'search').andCallFake(function () {
+            return {
+                success: function (callback) {
+                    callback({"results": [{}], "total_pages": 10})
+                }
+            }
+        });
+
+        scope.loadMovies();
+
+        expect(scope.totalPages).toEqual(10);
+        expect(scope.movies).toEqual([{}]);
+
+    });
 
 });
